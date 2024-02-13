a7d165a8c78ec15344004b528be5e171457005bf
Ridham Modh <ridhs193@gmail.com>
2024-02-13 18:56:16 -0500

    Update(Users): Updated README.md, removed console.logs

--- controllers/healthzcontroller.js
+++ controllers/healthzcontroller.js
@@ -5,6 +5,7 @@ exports.getHealthz = async (req, res) => {
     if (req.body && (Object.keys(req.body).length || Object.keys(req.query).length)) {
         res.status(400).send();
     }
+    res.setHeader('Cache-Control', 'no-cache');
     await sequelize.authenticate().then(() => {
         res.status(200).send();
     }).catch(() => {
