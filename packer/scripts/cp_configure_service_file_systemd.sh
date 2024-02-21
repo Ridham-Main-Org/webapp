ff37c7c08412100f8e64b7d329ee967e15f6ed70
Ridham Modh <ridhs193@gmail.com>
2024-02-21 15:02:18 -0500

    feat(packer-ga): add github action for packer build, make .env, .service file through script, make project,zone variable

--- packer/scripts/cp_configure_service_file_systemd.sh
+++ packer/scripts/cp_configure_service_file_systemd.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
 
-#Copy service file to systemd
-sudo cp /opt/my-webapp-copy/gcp-centos8.service /etc/systemd/system/
+# Define the service file path
+service_file="/opt/my-webapp-copy/gcp-centos8.service"
 
-#Reload systemd to recognize the new service file
+# Create the service file using a here document
+sudo tee "$service_file" > /dev/null <<-EOF
+[Unit]
+Description=CSYE 6225 App
+ConditionPathExists=/opt/my-webapp-copy/server.js
+After=network.target
+
+[Service]
+Type=simple
+User=csye6225
+Group=csye6225
+WorkingDirectory=/opt/my-webapp-copy
+ExecStart=/usr/bin/node /opt/my-webapp-copy/server.js
+Restart=always
+RestartSec=3
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=csye6225
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Contents of $service_file:"
+cat "$service_file"
+# Copy service file to systemd
+sudo cp "$service_file" /etc/systemd/system/
+
+echo "Contents of /etc/systemd/system/ directory after copying:"
+ls -l /etc/systemd/system/
+# Reload systemd to recognize the new service file
 sudo systemctl daemon-reload
 
+# Start and enable the service
 sudo systemctl start gcp-centos8.service
-
 sudo systemctl enable gcp-centos8.service
