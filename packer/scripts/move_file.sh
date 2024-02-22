27f616de45585feeb2303528c90a2df5b33fa2f8
Ridham Modh <ridhs193@gmail.com>
2024-02-21 19:44:02 -0500

    added logs

--- packer/scripts/move_file.sh
+++ packer/scripts/move_file.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo cp -r /tmp/my-webapp-copy.zip /opt/my-webapp-copy.zip
+
+# Print the contents of /tmp and /opt
+echo "Contents of /tmp:"
+ls -l /tmp
+
+echo "Contents of /opt:"
+ls -l /opt
