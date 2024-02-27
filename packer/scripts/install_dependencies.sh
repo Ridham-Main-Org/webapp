f9caba47bdba5b51fcac1f843e27c167044c3c52
Ridham Modh <ridhs193@gmail.com>
2024-02-27 01:42:57 -0500

    feat(remove-postgres): Remove postgres, edit .service file

--- packer/scripts/install_dependencies.sh
+++ packer/scripts/install_dependencies.sh
@@ -8,18 +8,18 @@ yes | sudo unzip -o my-webapp-copy.zip
 echo "Contents of /opt after unzipping:"
 ls -l /opt
 
-# Create the .env file with the specified contents
-sudo tee .env > /dev/null <<EOF
-DB_USER="postgres"
-DB_HOST="localhost"
-DB_NAME="UserDB"
-DB_PASSWORD="Timepass@1934"
-DB_PORT=5432
-EOF
+# # Create the .env file with the specified contents
+# sudo tee .env > /dev/null <<EOF
+# DB_USER="postgres"
+# DB_HOST="localhost"
+# DB_NAME="UserDB"
+# DB_PASSWORD="Timepass@1934"
+# DB_PORT=5432
+# EOF
 
-# Echo the contents of the .env file
-echo "Contents of .env file:"
-cat .env
+# # Echo the contents of the .env file
+# echo "Contents of .env file:"
+# cat .env
 
 # Install dependencies using npm
 sudo dnf module list nodejs
