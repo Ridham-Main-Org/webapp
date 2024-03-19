a4fd5c53cdec8c722dc603b0da6ae92887264dc0
Ridham Modh <ridhs193@gmail.com>
2024-03-18 20:32:24 -0400

    test logging

--- logger.js
+++ logger.js
@@ -17,10 +17,10 @@ const logger = winston.createLogger({
     )
 });
 
-if (process.env.NODE_ENV === 'testing') {
+// if (process.env.NODE_ENV === 'testing') {
     logger.add(consoleTransport);
-} else {
-    logger.add(new winston.transports.File({ filename: '/var/webapp/myapp.log' }));
-}
+// } else {
+//     logger.add(new winston.transports.File({ filename: '/var/webapp/myapp.log' }));
+// }
 
 module.exports = logger;
