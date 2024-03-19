11aa610556cb2ab411238cef1830b0b4355f6717
Ridham Modh <ridhs193@gmail.com>
2024-03-19 00:21:44 -0400

    test-ga-logger

--- api.test.js
+++ api.test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('./server');
-const logger = require('./logger');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
