158807b5c2cf7194979d3b840b8b5ef02d10e680
Ridham Modh <ridhs193@gmail.com>
2024-03-19 19:36:10 -0400

    update ops path, add  serverity code

--- packer/scripts/install_configure_ops_agent.sh
+++ packer/scripts/install_configure_ops_agent.sh
@@ -24,18 +24,23 @@ logging:
     my-app-receiver:
       type: files
       include_paths:
-        - /var/webapp/myapp.log
+        - /var/log/webapp/myapp.log
       record_log_file_path: true
   processors:
     my-app-processor:
       type: parse_json
       time_key: time
       time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+    move_severity:
+      type: modify_fields
+      fields:
+        severity:
+          move_from: jsonPayload.severity
   service:
     pipelines:
       default_pipeline:
         receivers: [my-app-receiver]
-        processors: [my-app-processor]
+        processors: [my-app-processor, move_severity]
 EOF
 )
 
