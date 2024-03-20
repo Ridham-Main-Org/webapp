537a6ffbe0a0f0aadabd77c34d8c8924b89a13f6
Ridham Modh <ridhs193@gmail.com>
2024-03-20 19:29:53 -0400

    logger,ops

--- logger.js
+++ logger.js
@@ -11,20 +11,16 @@ const logger = winston.createLogger({
     level: 'debug',
     format: winston.format.combine(
         // winston.format(info => ({ ...info, level: info.level.toUpperCase() }))(),
-        // winston.format.align(),
-        // winston.format.colorize(),
-        // winston.format.prettyPrint(),
         winston.format.json(),
-        // winston.format.errors({ stack: true }),
         winston.format.timestamp({
             format: 'YYYY-MM-DDTHH:mm:ss.SSSZ',
         }),
         // winston.format.printf(
         //     ({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`
         // ),
-        winston.format.printf(info => {
-            return JSON.stringify({ severity: info.level.toUpperCase(), time: info.timestamp,message: info.message });
-        })
+        // winston.format.printf(info => {
+        //     return JSON.stringify({ time: info.timestamp, message: info.message });
+        // })
     )
 });
 
