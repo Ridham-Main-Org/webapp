d4fba6ae3eb35b8fb5fabed23ba2c9b5d759ca4d
Ridham Modh <ridhs193@gmail.com>
2024-03-27 16:59:02 -0400

    updated logger

--- logger.js
+++ logger.js
@@ -1,27 +1,68 @@
-const winston = require('winston');
-
-// Define transports for console and file logging
-// const consoleTransport = new winston.transports.Console();
+const { createLogger, transports, format } = require('winston');
 
 const dotenv = require('dotenv');
-const { Logform } = require('winston');
 dotenv.config({ path: __dirname + "/.env" });
-
-let logTransports;
-
-if (process.env.NODE_ENV === 'testing') {
-    logTransports = new winston.transports.Console();
-} else {
-    logTransports = new winston.transports.File({ filename: '/var/log/webapp/myapp.log' });
+ 
+let loggerInstance = null;
+let envType = '';
+ 
+function initializeLogger() {
+  let logTransport;
+  let logFormat;
+ 
+  if (process.env.NODE_ENV === 'testing') {
+    logTransport = new transports.Console();
+    logFormat = format.json();
+  } else {
+    logTransport = new transports.File({ filename: '/var/log/webapp/myapp.log' });
+    logFormat = format.combine(
+      format.timestamp(),
+      format.json()
+    );
+  }
+ 
+  return createLogger({
+    level: 'debug',
+    format: logFormat,
+    transports: [logTransport]
+  });
+}
+ 
+export function getLogger() {
+  if (!loggerInstance) {
+    loggerInstance = initializeLogger(envType);
+  }
+  return loggerInstance;
 }
 
-const logger = winston.createLogger({
-    level: 'debug',
-    format: winston.format.combine(
-        winston.format.json(),
-        winston.format.timestamp(),
-    ),
-    transports: [logTransports]
-});
 
-module.exports = logger;
+
+// const winston = require('winston');
+
+// // Define transports for console and file logging
+// // const consoleTransport = new winston.transports.Console();
+
+// const dotenv = require('dotenv');
+// dotenv.config({ path: __dirname + "/.env" });
+
+// let logTransports;
+// let logFormat;
+
+// if (process.env.NODE_ENV === 'testing') {
+//     logTransports = new winston.transports.Console();
+//     logFormat = winston.format.json();
+// } else {
+//     logTransports = new winston.transports.File({ filename: '/var/log/webapp/myapp.log' });
+//     logFormat = winston.format.combine(
+//         winston.format.json(),
+//         winston.format.timestamp(),
+//     )
+// }
+
+// const logger = winston.createLogger({
+//     level: 'debug',
+//     format: logFormat,
+//     transports: [logTransports]
+// });
+
+// module.exports = logger;
