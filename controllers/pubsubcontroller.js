b09a7a12ee18086126fb63f7c3fbbadd4f320f1a
Ridham Modh <ridhs193@gmail.com>
2024-03-27 09:40:14 -0400

    fix(pubsub):update publisher

--- controllers/pubsubcontroller.js
+++ controllers/pubsubcontroller.js
@@ -5,15 +5,14 @@ const logger = require('../logger'); // Ensure logger is properly implemented
 // Create a Pub/Sub client; cache this for further use
 const pubSubClient = new PubSub();
 
-async function publishMessage(topicNameOrId, data) {
-    logger.debug("Inside publishMessage method");
+async function publishMessage(topicName, data) {
+    logger.debug(`Inside publishMessage method ${topicName}`);
     const dataBuffer = Buffer.from(data);
 
     try {
         // Publish the message and await the result
-        const [messageId] = await pubSubClient.topic(topicNameOrId).publishMessage(dataBuffer);
-        console.log(`Message ${messageId} published.`);
-        logger.info(`Message ${messageId} published`);
+        await pubSubClient.topic(topicName).publishMessage({ data: dataBuffer });
+        logger.info(`Message published`);
     } catch (error) {
         console.error(`Received error while publishing: ${error.message}`);
         logger.error("Received error while publishing message to topic inside publishMessage");
